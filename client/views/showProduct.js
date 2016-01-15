b7711dca59f60270a09e0f02c7b856c118e7d05c
Alain Goldman <Alain.goldman@gmail.com>
2016-01-15 00:25:43 -0500

    show now shows images

--- client/views/showProduct.js
+++ client/views/showProduct.js
@@ -1,5 +1,8 @@
 Template.showProduct.helpers({
     'seller': function(){
         return Meteor.users.findOne({_id : this.user });
+    },
+    'image': function(){
+    	return Images.find({'_id':{'$in':this.images}});
     }
 });
