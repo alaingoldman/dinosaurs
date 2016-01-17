5a3d4bfec7b39377931e89cf22c7b3c4ee4606ab
Alain Goldman <Alain.goldman@gmail.com>
2016-01-16 19:41:21 -0500

    setup image upload on edit page

--- client/views/editProduct.js
+++ client/views/editProduct.js
@@ -1,6 +1,47 @@
+Template.editProduct.events({
+	'change .myFileInput': function(event, template) {
+		 
+		 var productId = this._id
+	     FS.Utility.eachFile(event, function(file) {
+	      imageCount++;
+	       if(imageCount >= 5){
+	         FlashMessages.sendError("You can't upload more than 4 images");
+	        }else{
+	          Images.insert(file, function (err, fileObj) {
+	            if (err){
+	              FlashMessages.sendError("Images only");
+	              imageCount--;             
+	            } else {
+                  Meteor.call('editProductImages', productId, fileObj._id);
+	            	var imagesURL = {"profile.image": "/cfs/files/images/" + fileObj._id};
+	            	$(".myFileInput").val("");
+	            }
+	          });
+	        }
+
+	     });
+	},
+	"click .del": function(){
+		Meteor.call('removeThisImage', this._id, function(){
+	    imageCount--;
+	  });	
+	}
+});
+
 Template.editProduct.onCreated(function(){
     if(this.data.user === Meteor.userId()){
     }else{
     	Router.go('home');
     }
 });
+
+
+Template.editProduct.helpers({
+    'image': function(){
+    	return Images.find({'_id':{'$in':this.images}});
+    }
+});
+
+Template.editProduct.created = function() {
+  imageCount = this.data.images.length;
+};
