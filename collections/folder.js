4f07c6286bb081afaa66a19c6a1cc6b0f0b08d2d
Alain Goldman <Alain.goldman@gmail.com>
2016-01-22 02:40:37 -0500

    touched up collections

--- collections/folder.js
+++ collections/folder.js
@@ -1,2 +1,4 @@
 Folders = new Meteor.Collection('folders');
-// Holds the product image user association
+// The folders collection holds images before a product is submitted
+// upon submission of a product all images in the folder get associated 
+// to that product and the folder collection is deleted.
